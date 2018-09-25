68240d321933bb3a28a3cb5e136ff114cb2032d5
Wesley <wesleylondonwl@gmail.com>
2018-09-25 00:08:46 -0400

    finished gui functionality

--- python/gui.py
+++ python/gui.py
@@ -1,16 +1,17 @@
 
-
 from tkinter import *
 
 root = Tk()
+root.resizable(width = False, height = False)
 #county selection
 frame = Frame(root)
 frame.pack(fill = BOTH, pady = 10, padx = 50, side = 'left')
-countyLabel = Label(frame,text = 'County')
+
+countyLabel = Label(frame,text = 'County', font = 12)
 countyLabel.pack()
 
 #all counties in Florida in order corresponding to the site
-counties = {'ALACHUA','BAKER','BAY','BRADFORD','BREVARD','BROWARD','CALHOUN','CHARLOTTE','CITRUS','CLAY','COLLIER','COLUMBIA','DESOTO','DIXIE','DUVAL','ESCAMBIA','FLAGLER','FRANKLIN','GADSDEN','GILCHRIST','GLADES','GULF','HAMILTON','HARDEE','HENDRY','HERNANDO','HIGHLANDS','HILLSBOROUGH','HOLMES','INDIAN RIVER','JACKSON','JEFFERSON','LAFAYETTE','LAKE','LEE','LEON','LEVY','LIBERTY','MADISON','MANATEE','MARION','MARTIN','MIAMI-DADE','MONROE','NASSAU','OKALOOSA','OKEECHOBEE','ORANGE','OSCEOLA','PALM BEACH','PASCO','PINELLAS','POLK','PUTNAM','SANTA ROSA','SARASOTA','SEMINOLE','ST.JOHNS','ST.LUCIE','SUMTER','SUWANNEE','TAYLOR','UNION','VOLUSIA','WAKULLA','WALTON','WASHINGTON'}
+counties = ['ALACHUA','BAKER','BAY','BRADFORD','BREVARD','BROWARD','CALHOUN','CHARLOTTE','CITRUS','CLAY','COLLIER','COLUMBIA','DESOTO','DIXIE','DUVAL','ESCAMBIA','FLAGLER','FRANKLIN','GADSDEN','GILCHRIST','GLADES','GULF','HAMILTON','HARDEE','HENDRY','HERNANDO','HIGHLANDS','HILLSBOROUGH','HOLMES','INDIAN RIVER','JACKSON','JEFFERSON','LAFAYETTE','LAKE','LEE','LEON','LEVY','LIBERTY','MADISON','MANATEE','MARION','MARTIN','MIAMI-DADE','MONROE','NASSAU','OKALOOSA','OKEECHOBEE','ORANGE','OSCEOLA','PALM BEACH','PASCO','PINELLAS','POLK','PUTNAM','SANTA ROSA','SARASOTA','SEMINOLE','ST.JOHNS','ST.LUCIE','SUMTER','SUWANNEE','TAYLOR','UNION','VOLUSIA','WAKULLA','WALTON','WASHINGTON']
 #variable that will hold the value of whats selected
 countyChoice = StringVar(root)
 countyChoice.set('--not selected--')
@@ -23,18 +24,39 @@ scroll = Scrollbar(frame, orient= VERTICAL)
 scroll.config(command = countyMenu.yview)
 scroll.pack(side = 'right', fill = 'y')
 
-countyMenu.config(yscrollcommand = scroll.set ,width = 20, height = 10)
+countyMenu.config(yscrollcommand = scroll.set, width = 20, height = 10)
 #this frame area holds the rest of the widgets
 quotaFrame = Frame(root)
 quotaFrame.pack(fill = BOTH, pady = 10, padx = 30, side = 'right')
-quotaLabel = Label(quotaFrame,text = 'number of pages')
+quotaLabel = Label(quotaFrame, text = 'number of pages', font = 12)
 quotaLabel.grid(row = 0, column =0)
-quotaBox = Spinbox(quotaFrame, from_=1,to=500)
-quotaBox.config(width = 4)
+estimateLabel = Label(quotaFrame, text = 'Quota≈ 20 students', font = 12)
+estimateLabel.grid(row = 1, column= 0)
+def calculateQuota(quotaBox):
+    estimateLabel.config(text = 'Quota≈ '+str(int(quotaBox.get())*20) + ' students')
+
+quotaBox = Spinbox(quotaFrame, from_=1,to=500, state = 'readonly', command= lambda: calculateQuota(quotaBox))
+quotaBox.config(width = 4, font = 12)
 quotaBox.grid(row = 0, column = 1)
 
+
+errorLabel = Label(quotaFrame, font = 12, fg = 'red')
+errorLabel.grid(row=3,column = 0)
+
+def start(startButton):
+    if countyChoice.get() != '--not selected--':
+        countyChoice.set(countyMenu.curselection()[0])
+        print(countyChoice.get())
+        startButton.config(state = DISABLED)
+    else:
+        errorLabel.config(text = "Please choose a county to pull from")
+
+startButton = Button(quotaFrame, text = 'Start', command = lambda: start(startButton))
+startButton.config(width = 20, font = 14)
+startButton.grid(row=2, column = 0)
+
 def init():
-    root.title("CNADOWS")
+    root.title("CNA data web scraper")
 
     root.mainloop()
 
